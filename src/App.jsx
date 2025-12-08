b954130d135fca598f846a46869ffb0883798d0b
Keshav Dhawan <dhawankeshav03@gmail.com>
2025-12-08 13:17:53 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className="text-5xl font-bold text-green-600">
+        Tailwind v4 working
+      </h1>
+    </>
+  )
+}
+
+export default App
